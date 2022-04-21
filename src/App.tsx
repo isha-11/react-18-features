9c4921a653a0825a80bd88d145a8b68f6b740461
IshaBP <isha.pachkawade11@gmail.com>
2022-04-22 00:36:48 +0530

    main App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,53 @@
 import { GlobalStyle } from './GlobalStyles';
+import { routes } from './constants';
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes,
+  useParams,
+} from 'react-router-dom';
+import { Box, FlexBox } from 'react-styled-flex';
 
 export const App = () => {
   return (
     <>
       <GlobalStyle />
-      <main className='App'>
-        <a href='#'>Transition</a>
-        <a href='#'>Batching</a>
-        <a href='#'>Deferred Values</a>
-      </main>
+      <BrowserRouter basename='/react-18-features/'>
+        <Box as={'main'} height={'100%'}>
+          <Routes>
+            <Route path='/' element={<Index />}></Route>
+            <Route path='/:path' element={<Demo />}></Route>
+          </Routes>
+        </Box>
+      </BrowserRouter>
     </>
+  );
+};
+
+const Index = () => (
+  <>
+    <Link to={routes.transition}>Transition</Link>
+    <Link to={routes.batching}>Automatic Batching</Link>
+    <Link to={routes.deferredValue}>Deferred Value</Link>
+  </>
+);
+
+const Demo = () => {
+  const { path } = useParams();
+
+  return (
+    <FlexBox center gap={'3rem'} height={'100%'}>
+      <iframe
+        title={'React 17'}
+        src={`/react-18-features/pages/react-17.html#/${path}`}
+        height={'80%'}
+      ></iframe>
+      <iframe
+        title={'React 18'}
+        src={`/react-18-features/pages/react-18.html#/${path}`}
+        height={'80%'}
+      ></iframe>
+    </FlexBox>
   );
 };
