12a9e06e592fd0c641721f02f3673fb1207e01c2
IshaBP <isha.pachkawade11@gmail.com>
2022-04-22 00:36:08 +0530

    react 17 App.tsx

--- src/react-17/App.tsx
+++ src/react-17/App.tsx
@@ -1,12 +1,22 @@
 import { Transition } from './transition';
 import { GlobalStyle } from '../GlobalStyles';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { routes } from '../constants';
+import { Batching } from './batching';
+import { DeferredValue } from './deferred-value';
 
 export const App = () => {
   return (
     <>
       <GlobalStyle />
       <main className='App'>
-        <Transition />
+        <HashRouter>
+          <Routes>
+            <Route path={routes.transition} element={<Transition />} />
+            <Route path={routes.batching} element={<Batching />} />
+            <Route path={routes.deferredValue} element={<DeferredValue />} />
+          </Routes>
+        </HashRouter>
       </main>
     </>
   );
