5e94cc0e2505d059e2de29311394f83be45aab78
IshaBP <isha.pachkawade11@gmail.com>
2022-04-22 18:51:48 +0530

    deferred value react 17 and 18

--- src/react-17/DeferredValue.tsx
+++ src/react-17/DeferredValue.tsx
@@ -1,1 +1,64 @@
-export const DeferredValue = () => <div>Deferred Value</div>;
+import { ChangeEvent, useState } from 'react';
+import { FlexBox } from 'react-styled-flex';
+import styled from 'styled-components';
+
+export const DeferredValue = () => {
+  const [query, setQuery] = useState('');
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
+
+  return (
+    <FlexBox as='section' column gap={'1rem'} padding={'2rem 0'}>
+      <StyledInput type='text' value={query} onChange={handleChange} />
+      <Result query={query} />
+    </FlexBox>
+  );
+};
+
+const Result = ({ query }: { query: string }) => {
+  if (query === '') {
+    return <div></div>;
+  } else {
+    const rows = Array.from({ length: 5000 }, (_, i) => {
+      return (
+        <Card key={i}>
+          <img
+            src='data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K'
+            alt='logo'
+            height={200}
+            width={200}
+          />
+          <StyledQuery>{query}</StyledQuery>
+        </Card>
+      );
+    });
+
+    return (
+      <FlexBox center column gap={'1rem'}>
+        {rows}
+      </FlexBox>
+    );
+  }
+};
+
+const StyledInput = styled.input`
+  padding: 0.5rem;
+`;
+
+const Card = styled(FlexBox).attrs({
+  center: true,
+  column: true,
+  padding: '1rem',
+  width: '100%',
+})`
+  background-color: #20232a;
+  color: #61dafb;
+`;
+
+const StyledQuery = styled.div`
+  word-wrap: break-word;
+  white-space: pre-wrap;
+  word-break: break-word;
+`;
