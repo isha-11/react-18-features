8584edb4e8ed7c947e6bb11e71b5a661f778823d
IshaBP <isha.pachkawade11@gmail.com>
2022-04-22 12:15:12 +0530

    Batching react 17 and 18

--- src/react-17/batching.tsx
+++ src/react-17/batching.tsx
@@ -1,1 +1,32 @@
-export const Batching = () => <div>Batching</div>;
+import { useEffect, useState } from 'react';
+
+export const Batching = () => {
+  const [bool, setBool] = useState(true);
+  const [num, setNum] = useState(0);
+  const [date, setDate] = useState(() => Date.now());
+  const [ele, setEle] = useState<JSX.Element[]>([]);
+
+  const handleButtonClick = () => {
+    setTimeout(() => {
+      setBool(!bool);
+      setNum(num + 1);
+      setDate(Date.now());
+    }, 0);
+  };
+
+  useEffect(() => {
+    setEle([
+      ...ele,
+      <div key={`${date}_${num}_${bool}`}>
+        Bool: {bool.toString()} Num: {num} Date: {date}
+      </div>,
+    ]);
+  }, [bool, num, date]);
+
+  return (
+    <>
+      <button onClick={handleButtonClick}>Click</button>
+      {ele}
+    </>
+  );
+};
--- src/react-18/batching.tsx
+++ src/react-18/batching.tsx
@@ -1,1 +1,56 @@
-export const Batching = () => <div>Batching</div>;
+import { useEffect, useState } from 'react';
+import { Box, FlexBox } from 'react-styled-flex';
+
+export const Batching = () => {
+  const [bool, setBool] = useState(true);
+  const [num, setNum] = useState(0);
+  const [date, setDate] = useState(() => new Date().toString());
+  const [ele, setEle] = useState<JSX.Element[]>([]);
+
+  const handleButtonClick = () => {
+    setTimeout(() => {
+      setBool(!bool);
+      setNum(num + 1);
+      setDate(new Date().toString());
+    }, 0);
+  };
+
+  useEffect(() => {
+    setEle([
+      ...ele,
+      <Card
+        key={`${date}_${num}_${bool}`}
+        boolean={bool}
+        number={num}
+        date={date}
+      />,
+    ]);
+  }, [bool, num, date]);
+
+  return (
+    <>
+      <button onClick={handleButtonClick}>Click</button>
+      <FlexBox column gap={'1rem'}>
+        {ele}
+      </FlexBox>
+    </>
+  );
+};
+
+const Card = ({
+  boolean,
+  number,
+  date,
+}: {
+  boolean: boolean;
+  number: number;
+  date: string;
+}) => {
+  return (
+    <Box>
+      <div>Boolean: {boolean.toString()}</div>
+      <div>Number: {number}</div>
+      <div>Date: {date}</div>
+    </Box>
+  );
+};
