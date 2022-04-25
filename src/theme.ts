798d7650219caad3442033959ba41f6b52b7eebe
IshaBP <isha.pachkawade11@gmail.com>
2022-04-25 16:00:05 +0530

    Use polished to derive modified color

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,10 @@
+import { darken, lighten } from 'polished';
+
 export const theme = {
   color: {
     primary: '#61dafb',
-    primary10: '#35b6d9',
+    primary10: darken(0.15, '#61dafb'), //'#35b6d9',
     secondary: '#20232a',
-    secondary10: '#383a3e',
+    secondary10: lighten(0.15, '#20232a'), // #383a3e
   },
 };
