cbabe11fb920d4eaeb412aad14cda8b3d88d156c
IshaBP <isha.pachkawade11@gmail.com>
2022-04-29 18:44:03 +0530

    react-query setup

---
+++ src/common/queryClient.ts
@@ -0,0 +1,11 @@
+import { QueryClient } from 'react-query';
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
