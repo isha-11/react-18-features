e87a1cf28145846490e02092fa66d514f2b90e63
IshaBP <isha.pachkawade11@gmail.com>
2022-04-30 17:20:08 +0530

    Basic mobile layout

--- src/main/Demo.tsx
+++ src/main/Demo.tsx
@@ -9,24 +9,33 @@ export const Demo = () => {
   return (
     <FlexBox column height={'100%'} padding={'0 3rem 3rem 3rem'}>
       {demoPath ? <h1>{getTitleByRoute(`/${demoPath}`)}</h1> : null}
-      <FlexItem box center flex={1} gap={'3rem'} height={'100%'}>
+      <Content box center flex={1} gap={'3rem'} height={'100%'}>
         <Frame
           title={'React 17'}
           src={`/react-18-features/pages/react-17.html#/${demoPath}`}
-          height={'100%'}
         ></Frame>
         <Frame
           title={'React 18'}
           src={`/react-18-features/pages/react-18.html#/${demoPath}`}
-          height={'100%'}
         ></Frame>
-      </FlexItem>
+      </Content>
     </FlexBox>
   );
 };
 
 const Frame = styled.iframe`
   flex: 1;
+  height: 100%;
   border: none;
   box-shadow: 1px 1px 1rem 1px ${(props) => props.theme.color.shadow};
+`;
+
+const Content = styled(FlexItem)`
+  @media (max-width: 64rem) {
+    flex-direction: column;
+
+    ${Frame} {
+      width: 100%;
+    }
+  }
 `;
